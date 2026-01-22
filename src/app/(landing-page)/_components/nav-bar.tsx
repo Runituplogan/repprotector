dd8373881b600fd1536dc74a3923537fb4a73b94
Renn90 <106999299+Renn90@users.noreply.github.com>
2026-01-22 16:26:35 +0100

    fix: logo stretch fix

--- src/app/(landing-page)/_components/nav-bar.tsx
+++ src/app/(landing-page)/_components/nav-bar.tsx
@@ -5,9 +5,18 @@ import React from 'react'
 export default function NavBar() {
     return (
 
-        <nav className='py-[2rem] bg-white border-b-[.1rem] border-b-grey-100 fixed top-0 left-0 w-full z-40'>
+        <nav className='py-[2rem] bg-white border-b-[.1rem] border-b-grey-100 overflow-hidden fixed top-0 left-0 w-full z-40'>
             <Wrapper>
-                <Image src="/RepProtector Logo 1.png" className='' alt='RepProtector Logo 1' height={200} width={200} />
+                <div className="relative w-[200px] ">
+                    <Image
+                        src="/RepProtector Logo 1.png"
+                        alt='RepProtector Logo 1'
+                        height={200}
+                        width={200}
+                        sizes="200px"
+                        priority={true}
+                    />
+                </div>
             </Wrapper >
         </nav>
     )

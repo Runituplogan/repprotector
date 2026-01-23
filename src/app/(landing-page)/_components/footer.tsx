7a1c82c4bf2cf1803f8dbfe1487818cab87fc960
Renn90 <106999299+Renn90@users.noreply.github.com>
2026-01-23 15:09:46 +0100

    fix: footer responsiveness

--- src/app/(landing-page)/_components/footer.tsx
+++ src/app/(landing-page)/_components/footer.tsx
@@ -3,25 +3,28 @@
 import Link from "next/link";
 import { policies } from "../(policy-pages)/_data";
 import { PRIVACY_POLICY_ROUTES } from "../(policy-pages)/_utils/routes";
+import Wrapper from "@/src/components/wrapper";
 
 
 export default function Footer() {
   return (
     <footer id="footer" className="w-full flex flex-col gap-y-[3.2rem] border-b-[.1rem] border-b-primary items-center justify-center bg-light-background py-[2.4rem] text-center text-base font-medium text-grey-600">
-      <ul className="flex items-center gap-x-[5rem]">
-        {
-          policies.map((policy) => (
-            <li key={policy.id}>
-              <Link
-                href={`${PRIVACY_POLICY_ROUTES.getPolicyPage(policy.url)}`}
-              >
-                {policy.title}
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-      <p> © {new Date().getFullYear()} RepProtector, All Rights Reserved.</p>
+      <Wrapper className="space-y-[3.2rem] text-start md:text-center">
+        <ul className="flex w-full flex-col items-start justify-start md:justify-center md:items-center gap-y-[.8rem] md:gap-y-[0] md:gap-x-[5rem] md:flex-row">
+          {
+            policies.map((policy) => (
+              <li key={policy.id}>
+                <Link
+                  href={`${PRIVACY_POLICY_ROUTES.getPolicyPage(policy.url)}`}
+                >
+                  {policy.title}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+        <p> © {new Date().getFullYear()} RepProtector, All Rights Reserved.</p>
+      </Wrapper>
     </footer>
   );
 }
